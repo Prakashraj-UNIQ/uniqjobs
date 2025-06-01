4268bea5790b3443874c8be78127e0b9332b6dd4
Prakash Raj Pandiyarajan <prakash.raj@uniqtechnologies.co.in>
2025-06-01 20:06:26 +0530

    fix

--- uniqjobs/vite.config.js
+++ uniqjobs/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/uniqjobs/',
+  base: import.meta.env.VITE_BASE_PATH || '/uniqjobs/',
   plugins: [react(),tailwindcss()],
 })
