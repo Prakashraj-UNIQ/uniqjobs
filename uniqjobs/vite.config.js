1d90cb5eecacf1b11796dfeae2ffc083f6b519c7
Prakash Raj Pandiyarajan <prakash.raj@uniqtechnologies.co.in>
2025-06-01 21:37:31 +0530

    vite change

--- uniqjobs/vite.config.js
+++ uniqjobs/vite.config.js
@@ -1,25 +1,25 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// export default defineConfig({
-//   base: import.meta.env.VITE_BASE_PATH || '/uniqjobs/',
-//   plugins: [react(),tailwindcss()],
-// })
-
-
-
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import process from 'process'
 
-export default defineConfig(({ mode }) => {
-  // Load environment variables based on the current mode (development, production, etc.)
-  const env = loadEnv(mode, process.cwd())
-
-  return {
-    base: env.VITE_BASE_PATH || '/',
-    plugins: [react(), tailwindcss()],
-  }
+export default defineConfig({
+  base:  '/',
+  plugins: [react(),tailwindcss()],
 })
+
+
+
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// import process from 'process'
+
+// export default defineConfig(({ mode }) => {
+//   // Load environment variables based on the current mode (development, production, etc.)
+//   const env = loadEnv(mode, process.cwd())
+
+//   return {
+//     base: env.VITE_BASE_PATH || '/',
+//     plugins: [react(), tailwindcss()],
+//   }
+// })
